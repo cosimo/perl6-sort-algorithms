de4a4af855998429ca566422990e46f44849ec4a
Cosimo Streppone <cosimo@opera.com>
2011-08-17 20:32:40 +0200

    Added simpler version of merge function

--- lib/Algorithm/MergeSorter.pm
+++ lib/Algorithm/MergeSorter.pm
@@ -24,13 +24,54 @@ class Algorithm::MergeSorter is Algorithm::InstrumentedSorter {
         return if $lo >= $hi;
         my $mid = $lo / 2 + $hi / 2;
         $mid = $mid.Int;
+        say "Start merge-sort (lo=$lo, hi=$hi, mid=$mid)";
+
         $.merge-sort($lo, $mid);
+        say "After merge-sort left ($lo, $mid):  ", @!a.perl;
         $.merge-sort($mid + 1, $hi);
-        $.merge($lo, $mid, $hi);
+        say "After merge-sort right ($mid+1, $hi): ", @!a.perl;
+        $.merge-straightforward($lo, $mid, $hi);
+        say "After merge:            ", @!a.perl;
+    }
+
+    # Straightforward variant
+    method merge-straightforward ($lo is Int, $mid is Int, $hi is Int) {
+
+        # Copy both halves of a to auxiliary array b
+        for $lo .. $hi -> $i {
+            @!b[$i] = @!a[$i];
+            $.copies++;
+        }
+
+        # # Or...
+        # @!b[$lo .. $hi] = @!a[$lo .. $hi];
+        # $.copies += $hi - $lo + 1;
+
+        my $i = my $k = $lo;
+        my $j = $mid + 1;
+
+        # Copy back next-greatest element at each time
+        while $i <= $mid && $j <= $hi {
+            if @!b[$i] <= @!b[$j] {
+                @!a[$k++] = @!b[$i++];
+            }
+            else {
+                @!a[$k++] = @!b[$j++];
+            }
+            $.compares++;
+            $.copies++;
+        }
+
+        # Copy back remaining elements of first half (if any)
+        while $i <= $mid {
+            @!a[$k++] = @!b[$i++];
+            $.copies++;
+        }
+
     }
 
     # Efficient variant
-    method merge($lo is Int, $mid is Int, $hi is Int) {
+    method merge-efficient($lo is Int, $mid is Int, $hi is Int) {
 
         my Int $i = 0;
         my Int $j = $lo;
